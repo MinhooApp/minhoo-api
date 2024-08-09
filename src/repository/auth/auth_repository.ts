f776daba734532597b1a9244eaf949874656faa8
sergio Gil <kiron8088@gmail.com>
2024-08-09 01:00:21 +0000

    languaje null

--- src/repository/auth/auth_repository.ts
+++ src/repository/auth/auth_repository.ts
@@ -80,6 +80,7 @@ export const saveToken = async ({
   });
 
   const body = { auth_token: token, uuid: uuid };
+  console.log("HOLAAA "+uuid);
   const userTemp = await User.findOne({
     where: {
       id: userId,
