a3a88fe67cad06b45a23cda97bb12273dd90cf5a
sergio Gil <kiron8088@gmail.com>
2024-08-11 06:43:00 +0000

    comment corrections

--- src/_server/server.ts
+++ src/_server/server.ts
@@ -1,85 +1,102 @@
-import cors from "cors";
-import sequelize from "../_db/connection";
-import { Server as HttpServer } from "http";
+import cors from 'cors';
+import sequelize from '../_db/connection';
+import { Server as HttpServer } from 'http';
 import * as t from "../_models/association";
-import { Server as SocketIOServer } from "socket.io";
-import express, { Router, Application } from "express";
-import { socketController } from "../_sockets/socket_controller";
+import { Server as SocketIOServer } from 'socket.io';
+import express, { Router, Application } from 'express';
+import { socketController } from '../_sockets/socket_controller';
 
 console.log(t);
 
 interface Options {
-  port: number;
-  public_path?: string;
+    port: number;
+    public_path?: string;
 }
 class Server {
-  public readonly app = express();
-  private server: HttpServer;
-  private io: SocketIOServer;
-  private readonly port: number;
-  private readonly publicPath: string;
 
-  //private host: string;
+    public readonly app = express();
+    private server: HttpServer;
+    private io: SocketIOServer;
+    private readonly port: number;
+    private readonly publicPath: string;
 
-  constructor(options: Options) {
-    const { port, public_path = "public" } = options;
-    this.port = port;
-    this.publicPath = public_path;
-    this.server = new HttpServer(this.app);
-    this.io = new SocketIOServer(this.server, {
-      /* opciones */
-    });
-    this.middlewares();
-    this.dbConnection();
-    this.configure();
+    //private host: string;
 
-    /////////Sockets/////////////socketController
-    this.sockets();
-  }
 
-  async dbConnection() {
-    try {
-      // Se usa para crear las tablas de manera inicial
-      await sequelize.sync({ force: false });
-      console.log("✔️  Database Online !!!");
-    } catch (error: any) {
-      console.log(error);
-      throw new Error("🚫 " + error);
+    constructor(options: Options) {
+        const { port, public_path = 'public' } = options;
+        this.port = port;
+        this.publicPath = public_path;
+        this.server = new HttpServer(this.app);
+        this.io = new SocketIOServer(this.server, { /* opciones */ });
+        this.middlewares();
+        this.dbConnection();
+        this.configure();
+
+        /////////Sockets/////////////socketController
+        this.sockets();
+
     }
-  }
 
-  middlewares() {
-    //Cors
-    this.app.use(cors());
-    //Body Read
-    this.app.use(express.json());
 
-    //Public Folder
-    this.app.use(express.static("src/public"));
-  }
+    async dbConnection() {
+        try {
+            // Se usa para crear las tablas de manera inicial
+            await sequelize.sync({ force: false });
+            console.log('✔️  Database Online !!!')
+        } catch (error: any) {
+            console.log(error)
+            throw new Error('🚫 ' + error)
+        }
+    }
 
-  public setRoutes(ruoter: Router) {
-    this.app.use(ruoter);
-  }
 
-  private configure() {
-    //* Middlewares
-    this.app.use(express.json()); // raw
-    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+    middlewares() {
+        //Cors
+        this.app.use(cors())
+        //Body Read
+        this.app.use(express.json())
 
-    //* Public Folder
-    this.app.use(express.static(this.publicPath));
-  }
+        //Public Folder
+        this.app.use(express.static('src/public'))
 
-  sockets() {
-    this.io.on("connection", socketController);
-  }
-  /////////////////////////////////
-  listen() {
-    this.server.listen(this.port, () => {
-      console.log("Servidor corriendo en puerto", this.port);
-    });
-  }
+
+
+
+    }
+
+    public setRoutes(ruoter: Router) {
+        this.app.use(ruoter);
+    }
+
+
+    private configure() {
+
+        //* Middlewares
+        this.app.use(express.json()); // raw
+        this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
+        //* Public Folder
+        this.app.use(express.static(this.publicPath));
+
+
+
+    }
+
+    sockets() {
+        this.io.on('connection', socketController);
+
+
+    }
+    /////////////////////////////////
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+
+
 }
 
 export default Server;
+
