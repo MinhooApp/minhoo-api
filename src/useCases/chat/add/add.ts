8feff467efde86bb19037d4d0d3e0801b588063f
sergio Gil <kiron8088@gmail.com>
2024-08-12 18:49:55 -0500

    message notification

--- src/useCases/chat/add/add.ts
+++ src/useCases/chat/add/add.ts
@@ -1,16 +1,34 @@
-import { Request, Response, formatResponse, repository, socket } from '../_module/module';
-
+import {
+  Request,
+  Response,
+  formatResponse,
+  repository,
+  socket,
+  sendNotification,
+} from "../_module/module";
 
 export const sendMessage = async (req: Request, res: Response) => {
-    const { userId, message } = req.body;
-    try {
-        const response: any = await repository.initNewChat(req.userId, userId, message);
-        const newMessage = response.messages[response.messages.length - 1];
-        ////////Emit the chat/////
-        socket.emit('chat', newMessage);
-        return formatResponse({ res: res, success: true, body: response });
-    } catch (error) {
-        console.log(error);
-        return formatResponse({ res: res, success: false, message: error });
-    }
-}
+  const { userId, message } = req.body;
+  try {
+    const response: any = await repository.initNewChat(
+      req.userId,
+      userId,
+      message
+    );
+    const newMessage: any = response.messages[response.messages.length - 1];
+    ////////Emit the chat/////
+    socket.emit("chat", newMessage);
+    await sendNotification({
+      userId: userId,
+      interactorId: req.userId,
+      messageId: newMessage.chatId,
+
+      type: "message",
+      message: `wrote you a new message`,
+    });
+    return formatResponse({ res: res, success: true, body: response });
+  } catch (error) {
+    console.log(error);
+    return formatResponse({ res: res, success: false, message: error });
+  }
+};
