dce3774a308ea91028ef3963a43156d8c761353c
sergio Gil <kiron8088@gmail.com>
2024-08-22 23:32:49 -0500

    ok

--- src/_routes/estandar/auth/auth_routes.ts
+++ src/_routes/estandar/auth/auth_routes.ts
@@ -1,10 +1,15 @@
-import Router from 'express';
-import { validateEmail, verifyEmailCode, signUp, signUpWithImage, login } from '../../../useCases/auth/_controller/controller';
+import Router from "express";
+import {
+  validateEmail,
+  verifyEmailCode,
+  signUpWithImage,
+  login,
+} from "../../../useCases/auth/_controller/controller";
 const router = Router();
-router.post('/validate/email', validateEmail);
-router.post('/verify/email', verifyEmailCode);
-router.post('/', signUpWithImage);
-router.post('/image', signUpWithImage);
-router.post('/login', login);
+router.post("/validate/email", validateEmail);
+router.post("/verify/email", verifyEmailCode);
+router.post("/", signUpWithImage);
+router.post("/image", signUpWithImage);
+router.post("/login", login);
 
-export default router
+export default router;
