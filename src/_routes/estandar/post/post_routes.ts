a05014e22bd8221e5149fc480ce1ef70598cc7d2
sergio <kiron8088@gmail.com>
2025-01-03 21:20:02 -0300

    correction an alytycs

--- src/_routes/estandar/post/post_routes.ts
+++ src/_routes/estandar/post/post_routes.ts
@@ -5,6 +5,7 @@ import {
   gets,
   like,
   deletePost,
+  deletePostAdmin,
 } from "../../../useCases/post/_controller/controller";
 import { TokenValidation } from "../../../libs/middlewares/verify_jwt";
 const router = Router();
@@ -12,6 +13,7 @@ router.post("/", TokenValidation(), add);
 router.get("/", gets);
 router.put("/like/:id", TokenValidation(), like);
 router.get("/:id", get);
+router.delete("/admin/:id", TokenValidation([8088]), deletePost);
 router.delete("/:id", TokenValidation(), deletePost);
 
 export default router;
