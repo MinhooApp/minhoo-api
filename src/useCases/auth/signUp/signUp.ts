d75578f19b1d568d7000761102f2de47e9698032
sergio Gil <kiron8088@gmail.com>
2025-02-05 20:48:28 -0500

    test

--- src/useCases/auth/signUp/signUp.ts
+++ src/useCases/auth/signUp/signUp.ts
@@ -234,24 +234,27 @@ export const requestRestorePassword = async (req: Request, res: Response) => {
 
   try {
     const user = await repository.findByEmail(email);
-    if (user) {
-      await uRepository.update(user?.id, body);
-    }
-    const emailParams = {
-      subject: "reset password",
-      email: email,
-      htmlPath: "./src/public/html/email/reset_your_password.html",
-      replacements: [{ code: code, name: user!.name }],
-    };
+    if (user !== null && user !== undefined) {
+      await uRepository.update(user.id, body);
 
-    await sendEmail(emailParams);
+      const emailParams = {
+        subject: "reset password",
+        email: email,
+        htmlPath: "./src/public/html/email/reset_your_password.html",
+        replacements: [{ code: code, name: user!.name }],
+      };
+
+      await sendEmail(emailParams);
+    }
+
     return formatResponse({
       res: res,
       success: true,
       body: { created_temp_code: now },
     });
   } catch (error) {
-    return formatResponse({ res: res, success: false, message: error });
+    console.log(error);
+    return formatResponse({ res: res, success: false, message: "error" });
   }
 };
 
