b99f02ecb23f8b5cd926d44c727b9fe8484d8743
sergio Gil <kiron8088@gmail.com>
2025-03-29 06:03:24 -0400

    ok

--- src/useCases/notification/add/add.ts
+++ src/useCases/notification/add/add.ts
@@ -65,7 +65,7 @@ export const sendNotification = async (
       params.message,
       uuid,
       params.type,
-      params.interactorId! // <- puede devolver undefined
+      getFirstAvailableId(notificationData)! // <- puede devolver undefined
     );
   } catch (error) {
     console.error("Error al enviar la notificación:", error);
