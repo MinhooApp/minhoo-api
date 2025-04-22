dbf543d9993f161d712cb31f942a4d2d8b65fc90
sergio Gil <kiron8088@gmail.com>
2025-04-21 23:22:37 -0400

    test

--- src/_routes/estandar/user/user_routes.ts
+++ src/_routes/estandar/user/user_routes.ts
@@ -12,8 +12,8 @@ import {
 } from "../../../useCases/user/_controller/controller";
 router.get("/", TokenValidation(), gets);
 router.post("/follow", TokenValidation(), follow);
-router.get("/follows/:id?", TokenValidation(), follows);
-router.get("/followers/:id?", TokenValidation(), followers);
+router.get("/follows/:id?", follows);
+router.get("/followers/:id?", followers);
 router.get("/one/:id?", get);
 router.get("/myData", TokenValidation(), myData);
 router.get("/alert", TokenValidation(), activeAlerts);
