80bf6d5bd2b1301d3727724f3b939c0a80e4259c
sergio Gil <kiron8088@gmail.com>
2025-04-22 01:24:26 -0400

    test

--- src/useCases/chat/add/add.ts
+++ src/useCases/chat/add/add.ts
@@ -16,17 +16,17 @@ export const sendMessage = async (req: Request, res: Response) => {
       message
     );
     const messages = await repository.getChatByUser(req.userId, userId);
-    //const newMessage: any = response.messages[messages.length - 1];
+    const lastId = messages[messages.length - 1]?.id;
     ////////Emit the chat/////
     socket.emit("chat", response);
-    /*socket.emit("chats", userId);
-      await sendNotification({
-        userId: userId,
-        interactorId: req.userId,
-        messageId: newMessage.id,
-        type: "message",
-        message: `wrote you a new message`,
-      });*/
+    socket.emit("chats", userId);
+    await sendNotification({
+      userId: userId,
+      interactorId: req.userId,
+      messageId: lastId,
+      type: "message",
+      message: `wrote you a new message`,
+    });
 
     return formatResponse({ res: res, success: true, body: messages });
   } catch (error) {
