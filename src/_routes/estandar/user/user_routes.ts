9316e8cacf8cd2d6062180061ee96545a2ac3115
sergio <kiron8088@gmail.com>
2025-07-17 12:14:40 +0000

    test

--- src/_routes/estandar/user/user_routes.ts
+++ src/_routes/estandar/user/user_routes.ts
@@ -1,5 +1,7 @@
 import Router from "express";
 import { TokenValidation } from "../../../libs/middlewares/verify_jwt";
+import path from "path";
+import fs from "fs/promises";
 const router = Router();
 import {
   get,
@@ -15,4 +17,31 @@ router.get("/follows/:id?", TokenValidation(), follows);
 router.get("/followers/:id?", TokenValidation(), followers);
 router.get("/one/:id?", get);
 router.get("/myData", TokenValidation(), myData);
+router.get("/share/:id", async (req, res) => {
+  const postId = req.params.id;
+
+  const userAgent = req.headers["user-agent"] || "";
+  const isAndroid = /android/i.test(userAgent);
+  const isIOS = /iphone|ipad|ipod/i.test(userAgent);
+
+  const deepLink = `minhoo://profile/${postId}`;
+  const fallbackAndroid = "https://play.google.com/store/apps/details?id=aud.minhoo.io";
+  const fallbackIOS = "https://apps.apple.com/app/id123456789";
+  const fallback = isAndroid ? fallbackAndroid : fallbackIOS;
+
+  const filePath = path.join(__dirname, "../public/html/share/share.html");
+
+  try {
+    let html = await fs.readFile(filePath, "utf8");
+    html = html
+      .replace(/{{deepLink}}/g, deepLink)
+      .replace(/{{fallback}}/g, fallback);
+
+    res.setHeader("Content-Type", "text/html");
+    res.send(html);
+  } catch (error) {
+    console.error("❌ Error loading HTML file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 export default router;
