8d81f5b376057d153fff1f8a0d81311730cb844a
sergio <kiron8088@gmail.com>
2025-07-18 23:08:42 -0500

    al graphc reports

--- src/_routes/estandar/user/user_routes.ts
+++ src/_routes/estandar/user/user_routes.ts
@@ -10,6 +10,7 @@ import {
   follow,
   follows,
   followers,
+  activeAlerts,
 } from "../../../useCases/user/_controller/controller";
 router.get("/", TokenValidation(), gets);
 router.post("/follow", TokenValidation(), follow);
@@ -17,6 +18,7 @@ router.get("/follows/:id?", TokenValidation(), follows);
 router.get("/followers/:id?", TokenValidation(), followers);
 router.get("/one/:id?", get);
 router.get("/myData", TokenValidation(), myData);
+router.get("/alert", TokenValidation(), activeAlerts);
 router.get("/share/:id", async (req, res) => {
   const userId = req.params.id;
 
