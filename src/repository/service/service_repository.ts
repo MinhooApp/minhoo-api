ff2c474a8a3826a263614086b9b46222c9cb5f8d
sergio Gil <kiron8088@gmail.com>
2025-09-11 02:21:20 -0400

    test

--- src/repository/service/service_repository.ts
+++ src/repository/service/service_repository.ts
@@ -29,12 +29,12 @@ export const gets = async () => {
   return service;
 };
 
-export const history = async (userId?: number, canceled = true) => {
+export const history = async (userId?: number) => {
   if (userId != undefined) {
     const service = await Service.findAll({
       where: {
         userId: userId,
-        statusId: { [Op.notIn]: canceled ? [1] : [1, 5] },
+        statusId: { [Op.notIn]: [1] },
       },
       include: serviceInclude,
       order: [["service_date", "DESC"]],
